675ec75474f297f3da9d9a84989d2513446181e1
Afarx <98286758+Afarx1@users.noreply.github.com>
2025-03-26 11:43:21 -0300

    SCRUM-2
    SCRUM-2
    
    -Conectar el progreso de la base a Jira.
    -Agregar comentarios

--- spirit/main.ts
+++ spirit/main.ts
@@ -1,11 +1,14 @@
 // main.ts
 import { serve } from "https://deno.land/std/http/server.ts";
 
+
+// Text that will show during testing phase. Base for our web app
 const handler = (req: Request) => {
-  return new Response("Testeo de pagina web", {
+  return new Response("App web testing", {
     headers: { "content-type": "text/plain" },
   });
 };
 
+// Test
 console.log("Hello");
 serve(handler);
