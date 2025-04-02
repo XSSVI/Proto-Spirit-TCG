caa940ae1be7ec5671035fb4ecb79961ed4513d7
Afarx <98286758+Afarx1@users.noreply.github.com>
2025-04-02 00:03:22 -0300

    SCRUM-10
    -Integración con tailwind para la creación del front end.
    
    -Se agrego codigo para hacer la base del diseño de la pagina.

--- spirit/main.ts
+++ spirit/main.ts
@@ -1,14 +1,15 @@
-// main.ts
+//main.ts
+
+// Importing the `serve` function from Deno's standard HTTP server module
 import { serve } from "https://deno.land/std/http/server.ts";
 
-
-// Text that will show during testing phase. Base for our web app
-const handler = (req: Request) => {
-  return new Response("App web testing", {
-    headers: { "content-type": "text/plain" },
+const handler = async (req: Request) => {
+  const file = await Deno.readTextFile("index.html");
+  // Returns the content of "index.html" as an HTTP response
+  return new Response(file, {
+    headers: { "content-type": "text/html" },
   });
 };
 
-// Test
-console.log("Hello");
-serve(handler);
+// starts the HTTP server on port 8000 for live testing
+serve(handler, { port: 8000 });
